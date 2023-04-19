801911e5b61744f66d378741ace06b4956abbb61
Szymi <szymon@rudnikowski.pl>
2023-04-19 03:47:33 +0200

    Reload functionality.

--- src/commands/normal/ping.js
+++ src/commands/normal/ping.js
@@ -3,7 +3,7 @@ const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 module.exports = {
     name: "ping",
     aliases: ["pong"],
-    cooldown: 10000,//1 second = 1000 ms / cooldown will be disabled if you will put 0.
+    cooldown: 1000,//1 second = 1000 ms / cooldown will be disabled if you will put 0.
     run: async (client, message, args) => {
       message.reply(`Pong 🏓`)
     }
